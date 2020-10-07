9af9e775c89000c28c876549a51831a399e14862
50417 <sohil77@live.com>
2020-10-07 11:16:50 -0500

    starter tokens for Sampling simulink models

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -49,13 +49,17 @@ def interact_model(
 
     if length is None:
         length = hparams.n_ctx // 2
-    elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
-
-    with tf.Session(graph=tf.Graph()) as sess:
+        print(length)
+    #elif length > hparams.n_ctx:
+    #   raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+    config = tf.ConfigProto(device_count={'GPU': 0})
+    with tf.Session(graph=tf.Graph(),config=config) as sess:
         context = tf.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
+        raw_text = """Model {"""
+                   #input("Model prompt >>> ")
+        context_tokens = enc.encode(raw_text)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -66,23 +70,36 @@ def interact_model(
         saver = tf.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
         saver.restore(sess, ckpt)
-
+        file_to_save = open("sample.mdl",'w+')
+        file_to_save.write(raw_text)
         while True:
-            raw_text = input("Model prompt >>> ")
+            #raw_text = input("Model prompt >>> ")
             while not raw_text:
                 print('Prompt should not be empty!')
                 raw_text = input("Model prompt >>> ")
             context_tokens = enc.encode(raw_text)
+            #print(context_tokens)
+            #file_to_save.write(raw_text)	
             generated = 0
             for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
+                out = sess.run(output, feed_dict={context: [context_tokens for _ in range(batch_size)]})[:,
+                      len(context_tokens):]
+
                 for i in range(batch_size):
                     generated += 1
                     text = enc.decode(out[i])
+                    if "<|endoftext|>" in text:
+                        break
+                    file_to_save.write(text)
+	             
                     print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
                     print(text)
+            raw_text = text
+            if "<|endoftext|>" in text:
+                sep = "<|endoftext|>"
+                rest = text.split(sep, 1)[0]
+                file_to_save.write(rest)
+                break
             print("=" * 80)
 
 if __name__ == '__main__':
